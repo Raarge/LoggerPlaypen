5981a0f770add3f73e1a5ea39863585efd616f53
William Darin Campbell <animusdrogai@gmail.com>
2018-12-02 22:39:53 -0500

    Basic Setup of Log4Net

--- LoggerPlaypen/Program.cs
+++ LoggerPlaypen/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
 
 namespace LoggerPlaypen
 {
     class Program
     {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
+
+            var displayMessage = "Hello, user!";
+
+            _log.Info(displayMessage);
+
+            Console.WriteLine(displayMessage);
+
         }
     }
 }
